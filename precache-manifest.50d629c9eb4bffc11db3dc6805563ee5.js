d75f6b0fcbe83d31d78b9deb2a36d224417bfda1
sonia kechit <kechits@yahoo.com>
2025-11-13 23:39:46 +0100

    Updates

---
+++ precache-manifest.50d629c9eb4bffc11db3dc6805563ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1edc8cffca5171da0dff546a2d1ea4",
+    "url": "/BDD-Challenge/index.html"
+  },
+  {
+    "revision": "38ee0401c408f0a344f5",
+    "url": "/BDD-Challenge/static/css/main.2934dbcf.chunk.css"
+  },
+  {
+    "revision": "7cffdc3ca8772d6b9e0e",
+    "url": "/BDD-Challenge/static/js/2.46e58bf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BDD-Challenge/static/js/2.46e58bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ee0401c408f0a344f5",
+    "url": "/BDD-Challenge/static/js/main.bf8590f4.chunk.js"
+  },
+  {
+    "revision": "4a9f27099f4c64fbd5f7",
+    "url": "/BDD-Challenge/static/js/runtime-main.2ff6b40f.js"
+  }
+]);
